7d07a0145e9f6117dd8e5fef04c71be1a4db9218
ayushguha-31 <ayushguha31.com>
2024-12-11 22:20:50 +0530

    folder added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Generative_AI',  # No spaces
+    version='0.0.0',
+    author='Ayush Guha',
+    author_email='ayushguha31@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
